17ffaac06d238543013841f38b3283b34bfb6346
Tokka3 <ptokareff@gmail.com>
2023-02-05 16:50:13 +1100

    progress save

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 
 
+
 void PrintData(BYTE* data, int size)
 {
 	for (size_t i = 0; i < size; i++)
@@ -26,7 +27,7 @@ struct _MAIN_SCAN {
 struct VAC_FUNCTION {
 	std::vector<_MAIN_SCAN> mainscan_fns;
 	std::vector<PVOID> mainscan_ref_fns;
-	std::vector<PVOID> sorted_addresses;
+	std::vector<PVOID> sorted_addresses; // sorted address is just the combination of the two ab
 	std::string mod_name;
 };
 
@@ -46,19 +47,20 @@ std::map<UINT64, std::string> scanhashMap =
 	std::make_pair(0xc0590d2ae24ec3d3, "crash"),
 
 	//fully bypassed modules
-	std::make_pair(0xa00dbf237edd5a2f, "hwid"),
+	std::make_pair(0x807ee56c2128dc2d, "hwid"),
 	std::make_pair(0xea2af2e2bce3220a, "handle"),
 	std::make_pair(0xaab5578e266c1b61, "window"),
 	std::make_pair(0x68a58bb36a53a7a4, "filemapping"),
 
 	//unneeded modules
-	std::make_pair(0x2f28f009f9674643, "processInfo1"),
-	std::make_pair(0xb219318b5e3a74c3, "processInfo2"),
-	std::make_pair(0xda4778bb56d2989, "processInfo3"),
+	std::make_pair(0x84b281f10dcac7f0, "processInfo1"),
+	std::make_pair(0x2b8b9289eb65fced, "processInfo2"),
+	std::make_pair(0xb154c9c7bf1070ff, "processInfo3"),
+
 	std::make_pair(0xf0f0251921dfd9fa, "modsAndThreads1"),
 	std::make_pair(0x6b6eec243d579bae, "modsAndThreads2"),
-	std::make_pair(0xb59a2f53422e586e, "memScan"),
-	std::make_pair(0x8bded48bbea11906, "moreProcessInfo"),
+	std::make_pair(0xb67a7bf5058c3df9, "memScan"),
+	std::make_pair(0x248314d144e9bbb0, "moreProcessInfo"),
 	std::make_pair(0x4ff1f6019b33fd5f, "queryAndReadMem"),
 	std::make_pair(0xfdc2028fa3b8286f, "readMem"),
 
@@ -66,14 +68,19 @@ std::map<UINT64, std::string> scanhashMap =
 	std::make_pair(0xb34c2e74f57f4d0a, "SCQUERY"),
 	std::make_pair(0xa612337c5223f13a, "SNMP"),
 	std::make_pair(0x2153e378b10e88f5, "BOOTREGKEYS"),
-	std::make_pair(0x7e14cd062f6edb31, "SERVICEDEVICESTUFF"),
-	std::make_pair(0xd3dc7554776447f1, "CPUSTUFF"),
+	std::make_pair(0x7ed519a82f6eda98, "SERVICEDEVICESTUFF"),
+	std::make_pair(0xe3dd26051927a54c, "CPUSTUFF"),
+	
 
+		std::make_pair(0x32ccb640a26e7d68, "ENUMPROCHANDLE"),
 	//wip
 	std::make_pair(0xeee57d9442e9d36f, "USN"),
 	std::make_pair(0x463a5b2296ae616b, "SYSCALLSTUFF")
 };
 
+std::map<UINT64, int> scanRepeatCount {
+
+};
 void log_error(std::string str) {
 	std::cout << dye::red(str) << " " << std::hex << GetLastError() << std::endl;
 	Sleep(1000);
@@ -83,9 +90,10 @@ void log_error(std::string str) {
 int main() {
 
 	WIN32_FIND_DATA fd;
-	std::string path = "C:\\Users\\admin\\Desktop\\csgo shit\\moduledump\\good dump\\modules\\";
+	std::string path = "C:\\Users\\admin\\Desktop\\module_scan\\";
 	HANDLE hFind = FindFirstFile(std::string(path + "*.dll").c_str(), &fd);
 
+	int module_count = 0;
 	if (hFind != INVALID_HANDLE_VALUE) {
 		do {
 
@@ -139,17 +147,20 @@ int main() {
 				
 
 				PBYTE function_address = (PBYTE)(function_list[i]);
+			//	std::cout << "loop " << std::endl;
 
 				if (!function_address) break;
 
 				//std::cout << "function address: " << std::hex << DWORD(function_address) << std::endl;
 				function_address = (PBYTE)resolve_relative_address(base, (DWORD)function_address);
-			//	PrintData(function_address, 20);
+				if (*(BYTE*)(function_address) != 0xA1) break;
+
+			//	PrintData(function_address, 8);
 			
 				if (!function_address) break;
 
 				PVOID scan_add{};
-
+				module_count++;
 				for (function_address;; function_address += sizeof(byte)) { // looping through the bytes in each function to check for return
 
 					BYTE curr_byte = *(BYTE*)(function_address);
@@ -194,7 +205,7 @@ int main() {
 			//std::cout << function_count << " scan(s) found. " << std::endl;
 
 
-			// ------ this section adds all the addresses of the reference func and the actual mainscan's them selves to a list ------
+			// ------ this section adds all the addresses of the reference func and the actual mainscan's them selves to a list  so we can find function size by finding closest address------
 			for (_MAIN_SCAN add : vac_fncs.mainscan_fns) {
 				vac_fncs.sorted_addresses.emplace_back(add.main_scan_address);
 			}
@@ -217,38 +228,48 @@ int main() {
 			};
 
 		
-			std::cout << std::setw(20) << std::left <<  dye::light_green("mod name");
+			std::cout << std::setw(35) << std::left <<  dye::light_green("mod name");
 			std::cout << std::setw(10) << dye::light_green("count");
 			std::cout << std::setw(15) << dye::light_green("address");
 			std::cout << std::setw(15) << dye::light_green("ref address");
-			std::cout << std::setw(15) << dye::light_green("size") << std::endl;
-		
+			std::cout << std::setw(15) << dye::light_green("size");
+			std::cout << std::setw(25) << dye::light_green("repeat count");
+			std::cout << std::setw(25) << dye::light_green("hash");
+			std::cout << std::setw(15) << dye::light_green("identifier") << std::endl; ;
+
 			for (int i = 0; i < main_scan_count; i++) {
 				DWORD size = next_add(vac_fncs.mainscan_fns[i].main_scan_address) - DWORD(vac_fncs.mainscan_fns[i].main_scan_address);
-				std::cout << std::setw(20) << std::left <<  vac_fncs.mod_name;
-				std::cout << std::setw(10)  << vac_fncs.mainscan_fns[i].main_scan_count;
-				std::cout << std::setw(15)  << std::hex << vac_fncs.mainscan_fns[i].main_scan_address;
-				std::cout << std::setw(15)  << std::hex << vac_fncs.mainscan_ref_fns[i];
-				std::cout << std::setw(4) << std::hex << size;
-				
-			
+
 				UINT64 hash = 0;
 				UINT64* fnAddr = (UINT64*)vac_fncs.mainscan_fns[i].main_scan_address;
 				for (size_t i = 0; i < size / sizeof(UINT64); i++)
 				{
 					hash += fnAddr[i];
 				}
+
+				std::cout << std::setw(35) << std::left <<  vac_fncs.mod_name;
+				std::cout << std::setw(10)  << vac_fncs.mainscan_fns[i].main_scan_count;
+				std::cout << std::setw(15)  << std::hex << vac_fncs.mainscan_fns[i].main_scan_address;
+				std::cout << std::setw(15)  << std::hex << vac_fncs.mainscan_ref_fns[i];
+				std::cout << std::setw(15) << std::hex << size;
+				std::cout << std::setw(25)  << scanRepeatCount[hash];			
+				std::cout << std::setw(25) << std::hex << hash;
 				if (scanhashMap[hash].empty()) {
-					std::cout << dye::yellow("unknown") << std::endl;
+					std::cout << std::setw(15) <<  dye::yellow("unknown") << std::endl;
 				}
 				else {
-					std::cout << dye::yellow(scanhashMap[hash]) << std::endl;
+					std::cout << std::setw(15) << dye::yellow(scanhashMap[hash])  << std::endl;
+					 
 				}
+
+				scanRepeatCount[hash]++;
 				
 			}
+
 			vac_fncs.mainscan_fns.clear();
-			vac_fncs.sorted_addresses.clear();
 			vac_fncs.mainscan_ref_fns.clear();
+			vac_fncs.sorted_addresses.clear();
+		
 
 			
 
@@ -262,6 +283,8 @@ int main() {
 		} while (FindNextFile(hFind, &fd));
 		FindClose(hFind);
 	}
+
+	std::cout << "Scan done, found " << module_count << " modules " << std::endl;
 
 	system("pause");
 	return 0;
